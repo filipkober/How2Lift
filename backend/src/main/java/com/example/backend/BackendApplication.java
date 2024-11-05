43dd02e88022bc33eacf08f03cc9370643878826
Filip Kober <koberfilip2@gmail.com>
2024-11-05 16:52:20 +0100

    add file uploads

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,7 +1,10 @@
 package com.example.backend;
 
+import com.example.backend.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -11,6 +14,14 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
 
         System.out.println("Hello World");
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+//            storageService.deleteAll();
+            storageService.init();
+        };
     }
 
 }
